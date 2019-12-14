695be472f45848ff988b23466f1f304f738f3791
benneee <benedictiknkeonye@gmail.com>
2019-12-14 11:02:21 +0100

    Created new issue endpoints

--- MEANThing-BE/server.js
+++ MEANThing-BE/server.js
@@ -1,6 +1,100 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// Import model
+import Issue from './models/issue';
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+const router = express.Router();
+
+// Use CORS middleware to avoid connection issues since our Database will be hosted on another server
+app.use(cors());
+
+// Call the json method of the body-parser middleware to convert data to JSON
+app.use(bodyParser.json());
+
+// Connecting to the MongoDB Database
+mongoose
+  .connect('')
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const connection = mongoose.connection;
+
+// Listen for the 'open' event to tell us once there is a connection established with the database
+connection.once('open', () => {
+  console.log('Connection to the DB successful');
+});
+
+// Defining the Endpoints
+
+// Endpoint to get all the issues
+router.route('/issues').get((req, res) => {
+  Issue.find((err, issues) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(issues);
+    }
+  });
+});
+
+// Endpoint to get a particular issue by using its ID
+router.route('/issues/:id').get((req, res) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(issue);
+    }
+  });
+});
+
+// Endpoint to create a new issue on the server
+router.route('/issues/add').post((req, res) => {
+  let issue = new Issue(req.body);
+  issue
+    .save()
+    .then(issue => {
+      res.status(200).json({ issue: 'Added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send('Failed to create new issue');
+    });
+});
+
+// Endpoint to update an issue
+router.route('issues/update/:id').post((req, res, next) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (!issue) {
+      return next(new Error('Could not find issue'));
+    } else {
+      issue.title = req.body.title;
+      issue.responsible = req.body.responsible;
+      issue.description = req.body.descripion;
+      issue.severity = req.body.severity;
+      issue.status = req.body.status;
+
+      issue
+        .save()
+        .then(issue => {
+          res.json('Update successful');
+        })
+        .catch(err => {
+          res.status(400).send('Update failed');
+        });
+    }
+  });
+});
+
+// Endpoint to delete an issue
+route.route('/issues/delete/:id');
+app.use('/', router);
 
 app.listen(4000, () => console.log('Server running on port 4000'));
