70e811bdb9b5d666030447f418a90ccd3ef1a12b
benneee <benedictiknkeonye@gmail.com>
2019-12-15 15:36:46 +0100

    Front-end connected to the Back-end

---
+++ MEANThing-FE/src/app/issue.model.ts
@@ -0,0 +1,7 @@
+export class Issue {
+  title: string;
+  responsible: string;
+  description: string;
+  severity: string;
+  status?: string;
+}
--- MEANThing-FE/src/app/issues.service.ts
+++ MEANThing-FE/src/app/issues.service.ts
@@ -5,7 +5,54 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class IssuesService {
-  url = "https://meanthing-be.benneee.now.sh/issues";
+  url = "https://meanthing-be.benneee.now.sh";
 
   constructor(private http: HttpClient) {}
+
+  getIssues() {
+    return this.http.get(`${this.url}/issues`);
+  }
+
+  getSingleIssue(id: any) {
+    return this.http.get(`${this.url}/issues/${id}`);
+  }
+
+  createIssue(
+    title: string,
+    responsible: string,
+    description: string,
+    severity: string
+  ) {
+    const issue = {
+      title,
+      responsible,
+      description,
+      severity
+    };
+
+    return this.http.post(`${this.url}/issues/add`, issue);
+  }
+
+  updateIssue(
+    id: any,
+    title: string,
+    responsible: string,
+    description: string,
+    severity: string,
+    status: string
+  ) {
+    const issue = {
+      title,
+      responsible,
+      description,
+      severity,
+      status
+    };
+
+    return this.http.put(`${this.url}/issues/update/${id}`, issue);
+  }
+
+  deleteIssue(id: any) {
+    return this.http.delete(`${this.url}/issues/delete/${id}`);
+  }
 }
