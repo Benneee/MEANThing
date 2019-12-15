55b4f771bbc2f6969b36baeb4f8e9105b48e5d97
benneee <benedictiknkeonye@gmail.com>
2019-12-15 21:54:25 +0100

    WIP: Preparing the datatable for all issues

--- MEANThing-FE/src/app/components/create/create.component.ts
+++ MEANThing-FE/src/app/components/create/create.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { IssuesService } from "./../../issues.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"]
 })
 export class CreateComponent implements OnInit {
+  constructor(private issuesService: IssuesService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- MEANThing-FE/src/app/components/edit/edit.component.ts
+++ MEANThing-FE/src/app/components/edit/edit.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IssuesService } from "src/app/issues.service";
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  selector: "app-edit",
+  templateUrl: "./edit.component.html",
+  styleUrls: ["./edit.component.css"]
 })
 export class EditComponent implements OnInit {
+  constructor(private issuesService: IssuesService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
