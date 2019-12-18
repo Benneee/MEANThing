3d77fca7fddf7d07fab02cb6365d4c3214a8c1e8
benneee <benedictiknkeonye@gmail.com>
2019-12-18 22:31:00 +0100

    Completed the "Create Issue" form

--- MEANThing-FE/src/app/components/create/create.component.ts
+++ MEANThing-FE/src/app/components/create/create.component.ts
@@ -1,7 +1,12 @@
 import { IssuesService } from "./../../issues.service";
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-create",
@@ -10,19 +15,58 @@ import { Router } from '@angular/router';
 })
 export class CreateComponent implements OnInit {
   createIssueForm: FormGroup;
+  isLoading = false;
 
-  constructor(private issuesService: IssuesService, private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private issuesService: IssuesService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createIssue();
+  }
 
   createIssue() {
     this.createIssueForm = this.fb.group({
-      title: ['', Validators.required],
-      status: ['', Validators.required],
-      description: ['', Validators.required],
-      responsible: ['', Validators.required],
-      severity: ['', Validators.required]
+      title: ["", Validators.required],
+      description: "",
+      responsible: "",
+      severity: ""
     });
   }
 
+  submit() {
+    // console.log("form values: ", this.createIssueForm.value);
+    // Destructure out the parameters needed to create an issue and pass them to the addIssue function
+    const {
+      description,
+      responsible,
+      severity,
+      title
+    } = this.createIssueForm.value;
+    this.addIssue(description, responsible, severity, title);
+  }
+
+  addIssue(
+    description: string,
+    responsible: string,
+    severity: string,
+    title: string
+  ) {
+    this.isLoading = true;
+    this.issuesService
+      .createIssue(title, responsible, description, severity)
+      .subscribe(
+        (res: any) => {
+          if (res) {
+            console.log("res: ", res);
+            this.router.navigate(["/list"]);
+          }
+        },
+        (error: any) => {
+          console.log("err: ", error);
+        }
+      );
+  }
 }
